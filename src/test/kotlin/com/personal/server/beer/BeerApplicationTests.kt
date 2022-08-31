a9c454916a684f5974fab934b765b8d2849f4b1d
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2022-08-31 20:17:51 -0300

    Initial commit as per generated by Spring starter

---
+++ src/test/kotlin/com/personal/server/beer/BeerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.personal.server.beer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BeerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
