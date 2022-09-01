c982c8f3b8a027c3dae05ff64a64eae44f567910
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2022-08-31 22:04:47 -0300

    Introduce application service
    - In charge to perform domain business
    rules to serve orders.

---
+++ src/main/kotlin/com/personal/server/beer/domain/model/Beer.kt
@@ -0,0 +1,4 @@
+package com.personal.server.beer.domain.model
+
+data class Beer(val volume: Int, val brand: String, val many: Int) {
+}
---
+++ src/main/kotlin/com/personal/server/beer/domain/service/ApplicationOrderService.kt
@@ -0,0 +1,29 @@
+package com.personal.server.beer.domain.service
+
+import com.personal.server.beer.domain.model.Beer
+import org.springframework.stereotype.Service
+
+@Service
+class ApplicationOrderService {
+    private val beersStock: Map<String, Int> = mapOf(
+        "patagonia" to 10,
+        "zalaz" to 11,
+        "b&d" to 9,
+        "3#Orelhas" to 15
+    )
+
+    fun orderBeer(many: Int, volume: Int, brand: String): Beer {
+        val availability = beersStock.getOrDefault(brand, 0)
+
+        return Beer(volume, brand, availability)
+    }
+
+    fun list(): List<Beer> {
+        val beers = mutableListOf<Beer>()
+
+        beersStock.forEach { (k, u) -> beers.add(Beer(k.length, k, u)) }
+
+        return beers
+    }
+
+}
