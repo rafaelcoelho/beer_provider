0c68626d5f5b8b0bdfc94c8eb3c9d04df0502b38
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2022-09-01 22:29:02 -0300

    Inject via constructor
    - Decouple from application runtime IC
    - Promotes injection from testing scenarios

--- src/main/kotlin/com/personal/server/beer/infra/cfs/web/resources/BeerController.kt
+++ src/main/kotlin/com/personal/server/beer/infra/cfs/web/resources/BeerController.kt
@@ -2,7 +2,6 @@ package com.personal.server.beer.infra.cfs.web.resources
 
 import com.personal.server.beer.domain.model.Beer
 import com.personal.server.beer.domain.service.ApplicationOrderService
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,10 +12,7 @@ import javax.validation.constraints.Positive
 
 @RequestMapping("/order/v1")
 @RestController
-class BeerController {
-
-    @Autowired
-    private lateinit var appService: ApplicationOrderService
+class BeerController(val appService: ApplicationOrderService) {
 
     @PostMapping("/beer")
     fun orderABeer(@Valid req: OrderRequest): Beer {
