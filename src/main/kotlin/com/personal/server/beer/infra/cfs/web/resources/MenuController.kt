cc90041e0106d61a7a6bcf56f83893cb63b8d0f1
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2022-09-10 17:58:02 -0300

    Expose menu endpoint
    - Will be used to manage the menu

---
+++ src/main/kotlin/com/personal/server/beer/infra/cfs/web/resources/MenuController.kt
@@ -0,0 +1,23 @@
+package com.personal.server.beer.infra.cfs.web.resources
+
+import com.personal.server.beer.domain.service.ApplicationOrderService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/menu/v1")
+@RestController
+class MenuController(val appService: ApplicationOrderService) {
+
+    @PostMapping("/beer")
+    fun newBeerToStock(@RequestBody beerDto: BeerDto): ResponseEntity<Any> {
+        appService.newBeerOnStock(beerDto.many, beerDto.brand)
+        return ResponseEntity.status(HttpStatus.CREATED).build()
+    }
+
+}
+
+data class BeerDto(val many: Int, val brand: String)
