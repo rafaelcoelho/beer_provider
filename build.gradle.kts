47d95266c2e79ae19a3a1fbfbbaf2cea36abc150
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2022-09-12 20:14:30 -0300

    Add Gatling plugin
    - Gatling will be used to perform load testing
    against the service.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
+import io.gatling.gradle.LogHttp.FAILURES
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.3"
+    id("org.springframework.boot") version "2.7.3"
     id("io.spring.dependency-management") version "1.0.13.RELEASE"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
 
+    id("io.gatling.gradle") version "3.8.3.2"
     id("org.springframework.cloud.contract") version "3.1.3"
 }
 
@@ -14,13 +16,13 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 extra["springCloudVersion"] = "2021.0.3"
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+    implementation("org.springframework.boot:spring-boot-starter-data-rest")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -35,9 +37,9 @@ dependencies {
 }
 
 dependencyManagement {
-	imports {
-		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
-	}
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
 }
 
 tasks.withType<KotlinCompile> {
@@ -53,4 +55,9 @@ tasks.withType<Test> {
 
 contracts {
     setBaseClassForTests("com.personal.server.beer.BeerOrderManagementBaseTest")
+}
+
+gatling {
+    logLevel = "WARN"
+    logHttp = FAILURES
 }
