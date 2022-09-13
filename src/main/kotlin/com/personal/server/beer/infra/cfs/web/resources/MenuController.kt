2ae41108a88cf1f171c829e5de1a7507bf182fd0
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2022-09-13 08:26:28 -0300

    Random beer brand append
    - A random append on brand in order to support
    Gatling scenarios where the field in build statically.

--- src/main/kotlin/com/personal/server/beer/infra/cfs/web/resources/MenuController.kt
+++ src/main/kotlin/com/personal/server/beer/infra/cfs/web/resources/MenuController.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RequestMapping("/menu/v1")
 @RestController
@@ -14,7 +15,7 @@ class MenuController(val appService: ApplicationOrderService) {
 
     @PostMapping("/beer")
     fun newBeerToStock(@RequestBody beerDto: BeerDto): ResponseEntity<Any> {
-        appService.newBeerOnStock(beerDto.many, beerDto.brand)
+        appService.newBeerOnStock(beerDto.many, beerDto.brand + UUID.randomUUID())
         return ResponseEntity.status(HttpStatus.CREATED).build()
     }
 
