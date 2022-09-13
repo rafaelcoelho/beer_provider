e721e88cc6ab38fca608816dcfb1bf64b5e9540c
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2022-09-13 08:27:11 -0300

    Fix header on simulation scenario

--- src/gatling/kotlin/com/personal/server/beer/simulation/BeerSimulation.kt
+++ src/gatling/kotlin/com/personal/server/beer/simulation/BeerSimulation.kt
@@ -6,9 +6,10 @@ import io.gatling.javaapi.core.Simulation
 import io.gatling.javaapi.http.HttpDsl.http
 
 class BeerSimulation : Simulation() {
-    val httpProtocol = http // 4
+    val httpProtocol = http
         .baseUrl("http://localhost:8080")
-        .acceptHeader("application/json")
+        .acceptHeader("*/*")
+        .contentTypeHeader("application/json")
         .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
 
     val scn_list_beer = scenario("BeersMenu")
